30d5a659ed49b3863d8773cc8b313ef428bf4d24
Rohan Patankar <rohanpatankar926@gmail.com>
2022-05-06 22:51:37 +0530

    DVC pipeline initialization

---
+++ templates.py
@@ -0,0 +1,20 @@
+import os
+dirs=[
+    os.path.join("data","raw"),
+    os.path.join("data","processed"),
+    "notebooks",
+    "saved_models"
+      ]
+
+for dir in dirs:
+    os.makedirs(dir,exist_ok=True)
+    
+files=[
+    "dvc.yaml",
+    "params.yaml",
+    ".gitingnore",   
+]
+
+for file_ in files:
+    with open(file_,"w") as write:
+        pass
