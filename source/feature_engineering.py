4b299c2bd76d0a0292656400afccf5de680e75ae
Rohan Patankar <rohanpatankar926@gmail.com>
2022-05-08 10:42:41 +0530

    Feature Engineering Pipeline

---
+++ source/feature_engineering.py
@@ -0,0 +1,45 @@
+import pandas
+from get_data import GetData
+from data_preprocessing import Preprocessing 
+from load_data import LoadData
+from application_logging.logger import Applogger
+
+class FeatureEngineering:
+    def __init__(self):
+        self.logger=Applogger()
+        self.get_data=GetData()
+        self.load_data=LoadData()
+        
+    def data_(self,config_path):
+        self.config=self.get_data.read_params("H:/consignment pricing using mlops/params.yaml")
+        self.data=self.config["data"]["process"]
+        self.data=pandas.read_csv(self.data)
+        return self.data
+    
+    #outlier detection
+    def outlier_detection(self,data,colname):
+        self.data=data[data[colname]<=(data[colname].mean()+3*data[colname].std())]
+        return self.data
+        
+    def remove_outliers(self):
+        try:
+            logfile=open("logs/feature_engineering_log.txt","a+")
+            self.logger.log(logfile,"'remove_outliers' FUNCTION STARTED")
+            self.data=self.data_("H:/consignment pricing using mlops/params.yaml")
+            self.data0=self.outlier_detection(self.data,"line_item_value")
+            self.data1=self.outlier_detection(self.data0,"unit_of_measure_(per_pack)")
+            self.data2=self.outlier_detection(self.data1,"pack_price")
+            self.data3=self.outlier_detection(self.data2,"unit_price")
+            #self.data4=self.outlier_detection(self.data3,"days_to_process")
+            self.data=self.data3
+            self.logger.log(logfile,"removed outliers function compiled successfully")
+            return self.data
+        except Exception as e:
+            self.logger.log(logfile,"Exception occured in remove_outliers method"+str(e))
+            self.logger.log(logfile,"Error occured while removing outliers")
+        
+    def feature_engineering(self): 
+        pass
+
+object_=FeatureEngineering()
+object_.remove_outliers()
