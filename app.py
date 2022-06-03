d66d5cf5463bfa7f46efa296f476b1462f79b121
Rohan Patankar <rohanpatankar926@gmail.com>
2022-06-03 11:49:50 +0530

    Updated :)

--- app.py
+++ app.py
@@ -137,6 +137,7 @@ def train():
     except FileNotFoundError as e:
         error="Error occured while retraining 🤔🤔"
         error={"error":error}
-        return render_template("404.html",error=error)        
+        return render_template("404.html",error=error)
+port = int(os.environ.get("PORT", 5000))        
 if __name__=="__main__":
-    app.run(debug=True,port=5000)
+    app.run(debug=True,host="0.0.0.0",port=port)
