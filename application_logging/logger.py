1d51f85b727d673ec91c138d414e04c8077bf21e
Rohan Patankar <rohanpatankar926@gmail.com>
2022-06-22 18:28:49 +0530

    logger py file

--- application_logging/logger.py
+++ application_logging/logger.py
@@ -1,16 +1,3 @@
-# from datetime import datetime
-
-
-# class Applogger:
-#     def __init__(self):
-#         pass
-    
-#     def log(self,file_object,main_file,log_message):
-#         self.now=datetime.now()
-#         self.date=self.now.date()
-#         self.current_time=self.now.strftime("%H:%M:%S")
-#         file_object.write(str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message +"\n")
-#         main_file.write(str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message +"\n")
 import logging 
 import os
 import sys
