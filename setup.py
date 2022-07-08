7d935fd2478f24c358f781d59687e37b470c495f
Rohan Patankar <rohanpatankar926@gmail.com>
2022-07-08 14:47:10 +0530

    Update workflow

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
-setup(name="source/train_evaluate",version="1.0.0",description="Creating package for my machine learning project",url="https://consignmentpricing.herokuapp.com/",author="Rohan Patankar",license="MIT",packages=find_packages(include=["numpy","scikit-learn","pandas","joblib"]),python_requires='>=3')
+setup(name="source/train_evaluate", version="1.0.0", description="Creating package for my machine learning project", url="https://consignmentpricing.herokuapp.com/",
+      author="Rohan Patankar", license="MIT", packages=find_packages(include=["numpy", "scikit-learn", "pandas", "joblib"]), python_requires='>=3')
